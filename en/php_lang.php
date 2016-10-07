3c452d8f6c9edab90283c858279c38e8394e5f2c
Giancarlo Sciuto <Giancarlo Sciuto>
2016-10-07 20:16:03 +0200

    WIN

--- en/php_lang.php
+++ en/php_lang.php
@@ -1,36 +1,37 @@
 <?php
-    $lang["welcome_meta_title"]="A simply SEO Page Analyzer online for website - Seo Tester Online";
-    $lang["welcome_meta_description"]="Seo Tester Online is a website page seo analyzer online. You just simply submit a url and the Analyzer will check for you the right seo optimization: keywords coherency, speed performance, meta tags analysis and social rank! ";
-    $lang["welcome_h1"]="A simply SEO Page Analyzer online for website";
+    $lang["welcome_meta_title"]="SEO Page Analyzer online for website - Seo Tester Online";
+    $lang["welcome_meta_description"]="Make A free SEO analysis online of a web site: keywords analysis, page performances, html structure and popularity on social media. Submit a url and the Analyzer will check for you the right seo optimization.";
+    $lang["welcome_h1"]="SEO Analyzer online for website";
     $lang["welcome_opt_fullgrub"]="Site full scan (Cooming soon)";
     $lang["welcome_placeholder"]="http://www.websiteexample.com";
-    $lang['welcome_section_first_title']="Website seo optimization online";
+    $lang['welcome_section_first_title']="Website SEO optimization online";
     $lang['welcome_section_first_checkup']="Website SEO checkup";
     $lang['welcome_section_first_solve']="Fix all problems";
     $lang['welcome_section_first_gain']="Scale the search engine";
     $lang['welcome_section_second_title']="A complete SEO checkup online";
     $lang['welcome_section_second_tags_analyzer_title']="Tags Analyzer";
-    $lang['welcome_section_second_tags_analyzer_descr']="(Anteprima ricerca, descrizione, titolo, H1, H2, keywords url, url seo friendly, meta robots, robots.txt, sitemap)";
+    $lang['welcome_section_second_tags_analyzer_descr']="(Search preview search, description, title, H1, H2, URL keywords, SEO friendly URLs, meta robots, robots.txt, sitemap)";
     $lang['welcome_section_second_keywords_analyzer_title']="Keywords Analyzer";
     $lang['welcome_section_second_keywords_analyzer_descr']="(Keywords, keywords density analysis, microdata, title coherence, H1 coherence, text/code ratio, page size, alt on the images, title on a tags)";
     $lang['welcome_section_second_speed_analyzer_title']="Speed Analyzer";
     $lang['welcome_section_second_speed_analyzer_descr']="(time to above-the-fold load,time to full page load, the server configuration, the HTML structure of a page, and its use of external resources such as images, JavaScript, and CSS)";
     $lang['welcome_section_second_social_analyzer_title']="Social Analyzer";
-    $lang['welcome_section_second_social_analyzer_descr']="(Opengraph exam, twitter card, social plugin presence, rank on facebook)";
+    $lang['welcome_section_second_social_analyzer_descr']="(Opengraph exam, twitter card, social plugin presence, Facebook popularity)";
 $lang['welcome_section_third_title']="Why us?";
-    $lang['welcome_section_third_t1']="Be relax";
-    $lang['welcome_section_third_d1']="Now save time and money, in less than a minute you will have a complete <strong>seo checkup</strong>  of the commercial value of 400 € and maybe you will use that money for a cool vacation!";
+    $lang['welcome_section_third_t1']="Be relaxed";
+    $lang['welcome_section_third_d1']="Save time and money, in less than a minute you will have a complete <strong> seo checkup </ strong> of the commercial value of € 400 and
+    and you can use the money saved for a nice holiday!";
     $lang['welcome_section_third_t2']="Maniacs of the details";
     $lang['welcome_section_third_d2']="Will not escape anything in our seo tool, the site of which you will do the seo online analysis will be immediately explored to unearth every little flaw.";
-    $lang['welcome_section_third_t3']="Increase your turnover";
-    $lang['welcome_section_third_d3']="The SEO analysis today is very important, <strong>Seo Tester Online</strong> gives you the best tools to back up your competitors!";
+    $lang['welcome_section_third_t3']="Increase your online business.";
+    $lang['welcome_section_third_d3']="The seo analysis is vital today, <strong>Seo Tester Online</strong> gives you the best tools to overcome your competitors!";
 
-    $lang['welcome_section_third_t4']="We are nerds";
-    $lang['welcome_section_third_d4']="It 'a great <strong>seo analyzer</strong> for developers and SEO / specialist who want to <strong>test online a web site</strong>. Looking even to those who have just purchased a website and want to test it.";
+    $lang['welcome_section_third_t4']="Not just for nerds";
+    $lang['welcome_section_third_d4']="It 'a great <strong>seo analyzer</strong> for blogger, businesses, developers and SEO / specialist who want to <strong>test online a web site</strong>. Looking even to those who have just purchased a website and want to test it.";
     $lang['welcome_section_third_t5']="We love you";
-    $lang['welcome_section_third_d5']="Not only do we tell you what's wrong, seo online tester tells you step by step how to solve problems related to incorrect seo optimization of your website.";
+    $lang['welcome_section_third_d5']="Not only do we tell you what's wrong, SEO Tester Online tells you step by step how to solve problems related to incorrect seo optimization of your website.";
     $lang['welcome_section_third_t6']="All in one";
-    $lang['welcome_section_third_d6']="Are you tired of having to comb through many sites to be able to do something that you can comfortably on a single card? <strong>Seo Tester Online</strong> incorporates some features offered by Google as the Page Speed insights, a super accurate test to analyze the speed  performance of a web page.";
+    $lang['welcome_section_third_d6']="Are you tired of having to comb through many sites to be able to do something that you can comfortably on a single card? <strong>Seo Tester Online</strong> incorporates some features offered by Google as the Page Speed insights, a super accurate test to analyze the speed performances of a web page.";
 
     $lang['keyword_sugg_questions']=array(
         "how" => "how",
--- it/php_lang.php
+++ it/php_lang.php
@@ -15,17 +15,17 @@
     $lang['welcome_section_second_keywords_analyzer_descr']="(Keywords, keywords density analysis, microdata, title Coherence, H1 Coherence, text/code ratio, dimensioni pagina, alt sulle immagini, title sui tag a)";
     $lang['welcome_section_second_speed_analyzer_title']="Analisi delle performance";
     $lang['welcome_section_second_speed_analyzer_descr']="(Contenuti above the fold,tempo di caricamento completo della pagina,configurazione del server, struttura HTML di una pagina e il relativo utilizzo delle risorse esterne quali immagini, ottimizzazione dei file JavaScript e codici CSS)";
-    $lang['welcome_section_second_social_analyzer_title']="Social analisi";
+    $lang['welcome_section_second_social_analyzer_title']="Analisi Social";
     $lang['welcome_section_second_social_analyzer_descr']="(Analisi dell'opengraph, analisi twitter card, presenza di social plugin, grado di popolarità su facebook)";
     $lang['welcome_section_third_title']="Perchè scegliere noi?";
     $lang['welcome_section_third_t1']="Rimani comodo";
     $lang['welcome_section_third_d1']="Risparmia oggi tempo e denaro, in meno di un minuto avrai un <strong>seo checkup</strong> completo del valore commerciale di 400€ e magari utilizzerai quei soldi per farti una bella vacanza!";
     $lang['welcome_section_third_t2']="Maniaci del dettaglio";
     $lang['welcome_section_third_d2']="Non sfuggirà niente al nostro <strong>seo tool</strong>, il sito di cui vorrai fare l'<strong>analisi seo online</strong> verrà subito scandagliato per scovarne ogni piccolo difetto.";
-    $lang['welcome_section_third_t3']="Aumenta il tuo giro d'affari";
-    $lang['welcome_section_third_d3']="La seo analisi al giorno d'oggi è di vitale importanza, seo tester online ti offre i migliori strumenti per spalleggiare i tuoi competitor!";
+    $lang['welcome_section_third_t3']="Aumenta il tuo giro d'affari online";
+    $lang['welcome_section_third_d3']="La seo analisi al giorno d'oggi è di vitale importanza, seo tester online ti offre i migliori strumenti per superare i tuoi competitor!";
 
-    $lang['welcome_section_third_t4']="Solo per veri nerds";
+    $lang['welcome_section_third_t4']="Non solo per nerds";
     $lang['welcome_section_third_d4']="E' un ottimo <strong>seo tool</strong> per sviluppatori e SEO/specialist che vogliono <strong>testare un sito web online</strong>. Rivolto anche a coloro che hanno appena acquistato un sito web e vogliono testarlo.";
     $lang['welcome_section_third_t5']="Ti vogliamo bene";
     $lang['welcome_section_third_d5']="Non solo ti diciamo cosa non va, seo tester online ti spiega passo dopo passo come risolvere i problemi legati ad una non corretta <strong>ottimizzazione seo</strong> del tuo sito.";
